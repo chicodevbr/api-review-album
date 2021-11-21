1b05c2b3b77cddbaf09a8858baf1014245a1064b
chicodevbr <chicodevbr@gmail.com>
2021-11-21 13:52:00 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Album Review API...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}...`);
+});
