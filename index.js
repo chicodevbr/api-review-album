28fef20ff847404db7fb67bd764dd8b6f3aa3700
chicodevbr <chicodevbr@gmail.com>
2021-11-21 14:03:26 -0300

    some changes

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}...`);
+  console.log(`Server running on...`);
 });
