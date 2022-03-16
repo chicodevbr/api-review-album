2bc1717984a6b13eb966dfe8b3a3e17dee379730
chicodevbr <chicodevbr@gmail.com>
2022-03-16 16:28:45 -0300

    fix dotenv

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
-require('dotenv').config;
+dotenv.config();
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.use('/', indexRouter);
 app.use('/albums', albumRouter);
 
 const connection_string = process.env.MONGO_URI;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
