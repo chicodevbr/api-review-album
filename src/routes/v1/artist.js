9bdd4186df8c4f56d4e4155f9fbd0cdee816fb07
chicodevbr <chicodevbr@gmail.com>
2022-03-20 09:34:50 -0300

    adding getAll route

--- src/routes/v1/artist.js
+++ src/routes/v1/artist.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const artistController = require('../../controllers/artistController');
 const auth = require('../../middleware/auth');
 
-router.post('/artist', auth, artistController.add);
+router.get('/artist', artistController.getAll);
 router.get('/artist/:artistId', artistController.getArtistById);
+router.post('/artist', auth, artistController.add);
 
 module.exports = router;
