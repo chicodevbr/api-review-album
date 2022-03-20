9b014edf420f14074dca4a140fad024450837280
chicodevbr <chicodevbr@gmail.com>
2022-03-20 11:10:29 -0300

    update index

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('API Album Review version 1.0.3');
-  res.send('Hello API ALBUM REVIEW version 1.0.3');
+  console.log('API Album Review version 1.1.1');
+  res.send('Hello API ALBUM REVIEW version 1.1.1');
 });
 
 module.exports = router;
