5be8bf85f4efdd377add636e0b1b14fa59025456
chicodevbr <chicodevbr@gmail.com>
2022-03-20 19:11:24 -0300

    adding redirect to swagger docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,10 @@ const albumRoutes = require('./v1/albums');
 const artistRoutes = require('./v1/artist');
 const userRoutes = require('./v1/user');
 
+routes.get('/', (req, res) => {
+  // #swagger.ignore = true
+  res.redirect('/api/v1/docs');
+});
 routes.use('/api/v1', [indexRoute, albumRoutes, artistRoutes, userRoutes]);
 
 module.exports = routes;
