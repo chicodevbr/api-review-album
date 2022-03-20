59cdef59fab621b8a5671510a3c30be1cd14bbf9
chicodevbr <chicodevbr@gmail.com>
2022-03-19 21:45:44 -0300

    adding new routes

--- src/routes/v1/albums.js
+++ src/routes/v1/albums.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/albumControllers');
+const auth = require('../../middleware/auth');
 
 router.get('/album', controller.getAll);
-router.get('/album:id', controller.get);
+router.get('/album/:albumId', controller.getById);
 
-router.post('/album', controller.add);
+router.post('/album/:artistId', auth, controller.add);
 
-router.put('/album:id', controller.update);
+router.put('/album/:id', controller.update);
 
-router.delete('/album:id', controller.delete);
+router.delete('/album/:id', controller.delete);
 
 module.exports = router;
--- src/routes/v1/artist.js
+++ src/routes/v1/artist.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const artistController = require('../../controllers/artistController');
+const auth = require('../../middleware/auth');
 
-router.post('/artist', artistController.add);
+router.post('/artist', auth, artistController.add);
 
 module.exports = router;
