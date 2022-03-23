f18fb326b08a896179895093e27f451ccbc714fc
chicodevbr <chicodevbr@gmail.com>
2022-03-23 18:46:46 -0300

    adding updante and delete user routes

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -4,5 +4,7 @@ const userController = require('../../controllers/userController');
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
+router.put('/user/:userId', userController.update);
+router.delete('/user/:userId', userController.delete);
 
 module.exports = router;
