26dd28c13b69db2d05b8cf311e0d0ab8454d2491
chicodevbr <chicodevbr@gmail.com>
2022-03-24 19:06:53 -0300

    adding getReview by Id

--- src/routes/v1/review.js
+++ src/routes/v1/review.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const reviewController = require('../../controllers/reviewController');
 const auth = require('../../middleware/auth');
 
+router.get('/review/:reviewId', reviewController.getByReviewId);
 router.post('/review/:albumId', auth, reviewController.post);
 
 module.exports = router;
