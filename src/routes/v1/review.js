1f446b37a26169e11d76e63b45544d1fa40fc7cf
chicodevbr <chicodevbr@gmail.com>
2022-03-24 21:39:06 -0300

    adding get all reviews routes

--- src/routes/v1/review.js
+++ src/routes/v1/review.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const reviewController = require('../../controllers/reviewController');
 const auth = require('../../middleware/auth');
 
+router.get('/reviews', reviewController.getReviews);
 router.get('/review/:reviewId', reviewController.getByReviewId);
 router.get('/review/album/:albumId', reviewController.getByAlbumId);
 router.post('/review/:albumId', auth, reviewController.post);
