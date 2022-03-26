1b4f82ffc063d27bef5016a1f7ad307a61030687
chicodevbr <chicodevbr@gmail.com>
2022-03-26 17:40:58 -0300

    adding update routes

--- src/routes/v1/review.js
+++ src/routes/v1/review.js
@@ -8,5 +8,6 @@ router.get('/review/:reviewId', reviewController.getByReviewId);
 router.get('/review/album/:albumId', reviewController.getByAlbumId);
 router.get('/review/user/:userId', reviewController.getByUserId);
 router.post('/review/:albumId', auth, reviewController.post);
+router.put('/review/:reviewId', auth, reviewController.update);
 
 module.exports = router;
