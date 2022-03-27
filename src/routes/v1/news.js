41ab7366458794a6d5b5df8dae51132acfbba1fa
chicodevbr <chicodevbr@gmail.com>
2022-03-27 17:42:39 -0300

    adding updateLike routes

--- src/routes/v1/news.js
+++ src/routes/v1/news.js
@@ -7,5 +7,6 @@ router.get('/news', newsController.getNews);
 router.get('/news/:newsId', newsController.getNewsById);
 router.post('/news', auth, newsController.post);
 router.put('/news/newsId', auth, newsController.update);
+router.patch('/news/newsId', auth, newsController.updateLikes);
 
 module.exports = router;
