f8773e5f29e830aaa8a618caf53e29eb09290d96
chicodevbr <chicodevbr@gmail.com>
2022-03-27 17:06:40 -0300

    adding getNews and getNewsById

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,6 +1,29 @@
 const News = require('../models/news');
 const User = require('../models/user');
 
+exports.getNews = async (req, res) => {
+  // #swagger.tags = ['News']
+  try {
+    const news = await News.find();
+    if (!news) return res.status(404).json('There no news published yet.');
+    res.status(200).send(news);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+exports.getNewsById = async (req, res) => {
+  // #swagger.tags = ['News']
+  try {
+    const news = await News.findById(req.params.newsId);
+    if (!news) return res.status(404).send('News not found');
+
+    res.status(200).json(news);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 exports.post = async (req, res) => {
   // #swagger.tags = ["News"]
   try {
