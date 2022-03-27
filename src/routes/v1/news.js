1940593a2ea6d9d10add04849b7d97613788779e
chicodevbr <chicodevbr@gmail.com>
2022-03-27 17:49:16 -0300

    adding delete routes

--- src/routes/v1/news.js
+++ src/routes/v1/news.js
@@ -8,5 +8,6 @@ router.get('/news/:newsId', newsController.getNewsById);
 router.post('/news', auth, newsController.post);
 router.put('/news/newsId', auth, newsController.update);
 router.patch('/news/newsId', auth, newsController.updateLikes);
+router.delete('/news/newsId', auth, newsController.delete);
 
 module.exports = router;
