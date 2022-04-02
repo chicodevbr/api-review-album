dae5ce09d7ee558d02711fb3a7cf4f3bd905e1f0
chicodevbr <chicodevbr@gmail.com>
2022-04-02 14:13:04 -0300

    :test_tube: feat: adicioando test para endpoint get

---
+++ tests/app.test.js
@@ -0,0 +1,21 @@
+const app = require('../src/app');
+const supertest = require('supertest');
+const request = supertest(app);
+
+describe('GET /api/v1/', () => {
+  it('should return a response', async () => {
+    const response = await request.get('/api/v1/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, Welcome to API ALBUM REVIEW.');
+  });
+});
+
+// describe('GET /album', () => {
+//   it('responds with json', function (done) {
+//     request
+//       .get('/api/v1/album')
+//       .set('Accept', 'application/json')
+//       .expect('Content-Type', /json/)
+//       .expect(200, done);
+//   });
+// });
