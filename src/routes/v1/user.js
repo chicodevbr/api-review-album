9eec91cb6bf492418de911e1ef6fc4d4c2e1a832
chicodevbr <chicodevbr@gmail.com>
2022-04-02 15:47:50 -0300

    :sparkles: feat: adicionando verificações

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 const router = express.Router();
 const userController = require('../../controllers/userController');
 
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+router.post(
+  '/signup',
+  [
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    body('password').isLength({ min: 5 }),
+  ],
+  userController.signup
+);
+router.post(
+  '/login',
+  [check('name').not().isEmpty(), check('email').normalizeEmail().isEmail()],
+  userController.login
+);
 router.get('/users', userController.getAll);
 router.put('/user/:userId', userController.update);
 router.delete('/user/:userId', userController.delete);
