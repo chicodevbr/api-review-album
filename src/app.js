83c9dcdf66a81bbf85bcf676bf669d6fb3b00d45
chicodevbr <chicodevbr@gmail.com>
2022-04-08 15:58:59 -0300

    :boom: fix: cors

--- src/app.js
+++ src/app.js
@@ -5,13 +5,8 @@ const routes = require('./routes');
 const swaggerUI = require('swagger-ui-express');
 const swaggerFile = require('./swagger_documentation.json');
 
-app.use(cors());
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(cors());
 
 app.use(routes);
 app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
