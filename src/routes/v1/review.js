ec52014a51bfdfa9ff10f3a9683fe5800cec9b65
chicodevbr <chicodevbr@gmail.com>
2022-05-08 19:31:02 -0300

    :boom: fix: revertendo auth

--- src/routes/v1/review.js
+++ src/routes/v1/review.js
@@ -7,9 +7,9 @@ router.get('/reviews', reviewController.getReviews);
 router.get('/review/:reviewId', reviewController.getByReviewId);
 router.get('/review/album/:albumId', reviewController.getByAlbumId);
 router.get('/review/user/:userId', reviewController.getByUserId);
-router.post('/review/:albumId', auth, reviewController.post);
-router.patch('/review/:reviewId', auth, reviewController.updateLikes);
-router.put('/review/:reviewId', auth, reviewController.update);
+router.post('/review/:albumId', reviewController.post);
+router.patch('/review/:reviewId', reviewController.updateLikes);
+router.put('/review/:reviewId', reviewController.update);
 router.delete('/review/:reviewId', auth, reviewController.delete);
 
 module.exports = router;
